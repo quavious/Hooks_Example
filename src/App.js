7ef7328baa3b7366a6e65d5991fd52da4717d7a4
quavious <leenowon97@gmail.com>
2020-04-12 18:46:58 +0900

    useScroll

--- src/App.js
+++ src/App.js
@@ -2,51 +2,24 @@ import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-const useFadeIn = (duration=1, delay=0) => {
-  const element = useRef();
-  useEffect(() => {
-    if(element.current) {
-      const {current} = element;
-      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`
-      current.style.opacity = 1
-    }
-  }, [])
-  if(typeof duration !== "number") return;
-  return {ref:element, style:{opacity: 0}};
-}
-
-const useNetwork = onChange => {
-  const [status, setStatus] = useState(navigator.onLine)
-  const handleChange = () => {
-    if(typeof onChange === "function"){
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
+const useScroll = () => {
+  const [state, setState] = useState({x:0, y:0})
+  const onScroll = (event) => {
+    setState({x : window.scrollX, y: window.scrollY})
   }
   useEffect(() => {
-    window.addEventListener("online", handleChange)
-    window.addEventListener("offline", handleChange)
-    return () => {
-      window.removeEventListener("online", handleChange)
-      window.removeEventListener("offline", handleChange)
-    }
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, [])
-  return status
+  return state
 }
 
 const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We just went online" : "We are offline")
-  }
-
-  const onLine = useNetwork(handleNetworkChange);
-  const fadeIn = useFadeIn(1,2);
-  const fadeInP = useFadeIn(5,10)
+  const {y} = useScroll();
   return (
-    <div className="App">
-      <h2 {...fadeIn}>Hello</h2>
-      <p {...fadeInP}>lorem lalala</p>
-      <h1>{onLine ? "online" : "offline"}</h1>
+    <div className="App" style={{height: '1000vh'}}>
+      <h2 style={{position: "fixed", color: y > 100 ? "red" : "blue"}}>Hello</h2>
+      
     </div>
   );
 }
