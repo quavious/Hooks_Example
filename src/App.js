11fcdb34785c5fecf5eb721d5b452d4d54445143
quavious <leenowon97@gmail.com>
2020-04-12 17:37:18 +0900

    useConfirm and usePreventLeave

--- src/App.js
+++ src/App.js
@@ -2,29 +2,45 @@ import React, { useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-const useClick = (onClick) => {
-  const element = useRef();
-  
-  useEffect(() => {
-    if(element.current){
-      element.current.addEventListener("click", onClick)
-      console.log("hello")
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if(!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if(!onCancel && typeof onCancel !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if(window.confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
     }
-    return () => {
-      if(element.current)
-        element.current.removeEventListener("click", onClick)
-    }
-  },[])
-  if(typeof onClick !== "function") return;
-  return element
+  }
+  return confirmAction
+}
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  } 
+  const enablePrevent = () => window.addEventListener("beforeunload", listener)
+  const disablePrevent = () => window.removeEventListener("beforeunload", listener)
+  return {enablePrevent, disablePrevent}
 }
 
 const App = () => {
-  const sayHello = () => console.log("say hello")
-  const title = useClick(sayHello); 
+  const deleteWorld = () => console.log("Deleting the world")
+  const abort= () => console.log("Aborted")
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort)
+  
+  const {enablePrevent, disablePrevent} = usePreventLeave()
   return (
     <div className="App">
-      <h2 ref={title}>Hello</h2>
+      <h2>Hello</h2>
+      <button onClick={confirmDelete}>Delete the World</button>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>unProtect</button>
     </div>
   );
 }
