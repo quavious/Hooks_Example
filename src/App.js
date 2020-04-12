024ff60b20a7fc7c9a815f7e42a1d61704e1b911
quavious <leenowon97@gmail.com>
2020-04-12 18:40:15 +0900

    useFadeIn useNetwork

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-const useConfirm = (message = "", onConfirm, onCancel) => {
-  if(!onConfirm || typeof onConfirm !== "function") {
-    return;
-  }
-  if(!onCancel && typeof onCancel !== "function") {
-    return;
-  }
-  const confirmAction = () => {
-    if(window.confirm(message)) {
-      onConfirm();
-    } else {
-      onCancel();
+const useFadeIn = (duration=1, delay=0) => {
+  const element = useRef();
+  useEffect(() => {
+    if(element.current) {
+      const {current} = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`
+      current.style.opacity = 1
     }
-  }
-  return confirmAction
+  }, [])
+  if(typeof duration !== "number") return;
+  return {ref:element, style:{opacity: 0}};
 }
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
-  } 
-  const enablePrevent = () => window.addEventListener("beforeunload", listener)
-  const disablePrevent = () => window.removeEventListener("beforeunload", listener)
-  return {enablePrevent, disablePrevent}
+const useNetwork = onChange => {
+  const [status, setStatus] = useState(navigator.onLine)
+  const handleChange = () => {
+    if(typeof onChange === "function"){
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  }
+  useEffect(() => {
+    window.addEventListener("online", handleChange)
+    window.addEventListener("offline", handleChange)
+    return () => {
+      window.removeEventListener("online", handleChange)
+      window.removeEventListener("offline", handleChange)
+    }
+  }, [])
+  return status
 }
 
 const App = () => {
-  const deleteWorld = () => console.log("Deleting the world")
-  const abort= () => console.log("Aborted")
-  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort)
-  
-  const {enablePrevent, disablePrevent} = usePreventLeave()
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline")
+  }
+
+  const onLine = useNetwork(handleNetworkChange);
+  const fadeIn = useFadeIn(1,2);
+  const fadeInP = useFadeIn(5,10)
   return (
     <div className="App">
-      <h2>Hello</h2>
-      <button onClick={confirmDelete}>Delete the World</button>
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>unProtect</button>
+      <h2 {...fadeIn}>Hello</h2>
+      <p {...fadeInP}>lorem lalala</p>
+      <h1>{onLine ? "online" : "offline"}</h1>
     </div>
   );
 }
